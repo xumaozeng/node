f99a5c82f9538e623500b5984d745ce3e8f9af72
xumaozeng <15611895341@163.com>
2020-12-14 23:25:00 +0800

    [ADD] generator

--- async/index.js
+++ async/index.js
@@ -28,3 +28,46 @@ exports.promise = () => {
 };
 
 // Generator
+exports.generator = () => {
+  const generator = function* (name) {
+    yield promise(name + 1);
+    yield promise(name + 2);
+    yield promise(name + 3);
+  };
+  let co = generator => {
+    if ((it = generator.next().value)) {
+      it.then(res => {
+        co(generator);
+      });
+    } else {
+      return;
+    }
+  };
+  co(generator("Co-generator"));
+};
+
+// async-await
+exports.asyncAwait = async () => {
+  await promise("Async/Await1");
+  await promise("Async/Await2");
+  await promise("Async/Await3");
+};
+
+// event
+exports.event = async () => {
+  const asyncFun = name => event => {
+    setTimeout(() => {
+      logTime(name);
+      event.emit("end");
+    }, 100);
+    return event;
+  };
+
+  const ary = [asyncFun("event1"), asyncFun("event2")];
+
+  const { EventEmitter } = require("events");
+  const event = new EventEmitter();
+  let i = 0;
+  event.on("end", () => i < ary.length && ary[i++](event));
+  event.emit("end");
+};
--- async/serial/__test__/index.spec.js
+++ async/serial/__test__/index.spec.js
@@ -1,4 +1,10 @@
-const { callback, promise } = require("../../index");
+const {
+  callback,
+  promise,
+  generator,
+  asyncAwait,
+  event
+} = require("../../index");
 
 /* test("callback", done => {
   callback();
@@ -6,8 +12,26 @@ const { callback, promise } = require("../../index");
   setTimeout(done, 1000);
 }); */
 
-test("promise", done => {
+/* test("promise", done => {
   promise();
+  // 延迟1秒
+  setTimeout(done, 1000);
+}); */
+
+/* test("generator", done => {
+  generator();
+  // 延迟1秒
+  setTimeout(done, 1000);
+}); */
+
+/* test("asyncAwait", done => {
+  asyncAwait();
+  // 延迟1秒
+  setTimeout(done, 1000);
+}); */
+
+test("event", done => {
+  event();
   // 延迟1秒
   setTimeout(done, 1000);
 });
