477f52cc2bb4bdfa1c5a0e381937f1a79da7c979
xumaozeng <15611895341@163.com>
2021-03-24 23:48:01 +0800

    作业6

--- node_practice/ex06/index.js
+++ node_practice/ex06/index.js
@@ -1,22 +1,26 @@
-const crypto = require('crypto')
+const crypto = require("crypto");
+const { Buffer } = require("buffer");
 module.exports.createToken = token => {
-    const ary = token.split('.')
-    if (ary.length !== 3) {
-        return
-    }
+  const ary = token.split(".");
+  if (ary.length !== 3) {
+    return;
+  }
 
-    return {
-        getExp: () => {
-            // ##BEGIN## 代码已加密
-gywgywgywgywgywgywgywgywgywgywgywgywgdqgdUgdvgdegdwgywgdYgdggRcgdPgdUgdggdcgywgcRgywgcwg9kg9cg9qgqDgdYgdggdmgdegd9gqRgdvgd9gRygywgcPgd=gddgddgd9gdmgqDgddgdmgdUgdDgqRgdggdmgRcg9YgqYg9mgqlgywgqdgdygdggdegd9gq=gqegqdgqkgqk
-gywgywgywgywgywgywgywgywgywgywgywgywgdmgd9gdwgd=gdmgdvgywgdYgdggRcgdPgdUgdggdcgqDgd9gRqgdY
-            // ##END##
-        },
+  return {
+    getExp: () => {
+      // ##BEGIN## 代码已加密
+      const dateStr = Buffer.from(ary[1], "base64").toString("utf-8");
+      const { exp } = JSON.parse(dateStr);
+      return exp;
+      // ##END##
+    },
 
-        verify: key => {
-            const hmac = crypto.createHmac('SHA256', key).update(ary[0]+ '.' +  ary[1]).digest('base64');
-            return hmac === ary[2] + '='
-            
-        }
+    verify: key => {
+      const hmac = crypto
+        .createHmac("SHA256", key)
+        .update(ary[0] + "." + ary[1])
+        .digest("base64");
+      return hmac === ary[2] + "=";
     }
-}
+  };
+};
