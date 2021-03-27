b3bdde08686c0685c6e66e92a6f0c8cf8fb6adda
xumaozeng <15611895341@163.com>
2021-03-27 11:37:02 +0800

    作业07

--- node_practice/ex07/index.js
+++ node_practice/ex07/index.js
@@ -1,25 +1,30 @@
-const fs = require('fs')
+const fs = require("fs");
 module.exports.createLoader = config => {
-    const loader = (scanFolder, cb) => {
-        const files = fs.readdirSync(scanFolder);
-        files.forEach(filename => {
-            filename = filename.replace(".js", "");
-            const file = require(scanFolder + "/" + filename);
-            cb(filename, file);
-        })
-    }
+  const loader = (scanFolder, cb) => {
+    const files = fs.readdirSync(scanFolder);
+    files.forEach(filename => {
+      filename = filename.replace(".js", "");
+      const file = require(scanFolder + "/" + filename);
+      cb(filename, file);
+    });
+  };
 
-    return {
-        initFunction: scanFolder => {
-            const ret = {}
-            // ##BEGIN## 代码已加密
-gywgywgywgywgywgywgywgywgywgywgywgywgdPgdUgdggdcgd9gdmgqRgdegdqgdggdvgcYgdUgdPgdcgd9gdmgqlgywgqRgddgdngdPgd9gdvgdggdDgd9gqlgywgdqgdmgd9gdggdwgd9gcYgd=gdvgqkgywgcRgckgywgRd
-gywgywgywgywgywgywgywgywgywgywgywgywgywgywgywgywgdqgdUgdvgdegdwgywgddgd=gdvgdqgywgcRgywgdqgdmgd9gdggdwgd9gcYgd=gdvgqRgdqgdUgdvgddgdngdRgqk
-gywgywgywgywgywgywgywgywgywgywgywgywgywgywgywgywgdmgd9gdwg9YgddgdngdPgd9gdvgdggdDgd9g9mgywgcRgywgddgd=gdvgdq
-gywgywgywgywgywgywgywgywgywgywgywgywgRkgqk
-            // ##END##
-            return ret
-        }
+  return {
+    initFunction: scanFolder => {
+      const ret = {};
+      // ##BEGIN## 代码已加密
+      // 方法1
+      /* const files = fs.readdirSync(scanFolder);
+      files.forEach(filename => {
+        filename = filename.replace(".js", "");
+        ret[`${filename}`] = require(scanFolder + "/" + filename)(config);
+      }); */
+      // 方法2：使用loader
+      loader(scanFolder, function (filename, file) {
+        ret[`${filename}`] = file(config);
+      });
+      // ##END##
+      return ret;
     }
-}
-
+  };
+};
