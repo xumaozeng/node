d7544d8ae87e9de2a96db36214b1985f71b731ad
xumaozeng <15611895341@163.com>
2021-05-03 22:46:13 +0800

    二叉树锯齿形层次遍历

---
+++ interview/字节-懂车帝/zigzagLevelOrder.js
@@ -0,0 +1,33 @@
+/**
+ * 二叉树的锯齿形层次遍历
+ */
+
+function zigzagLevelOrder(root) {
+  if (!root) return [];
+
+  const result = [];
+  const nodeQueue = [root];
+
+  let isOrderLeft = true;
+
+  while (nodeQueue.length) {
+    let levelList = [];
+    const size = nodeQueue.length();
+
+    for (let i = 0; i < size; i++) {
+      const node = nodeQueue.shift();
+      if (isOrderLeft) {
+        levelList.push(node.val);
+      } else {
+        levelList.unshift(node.val);
+      }
+      if (node.left) nodeQueue.push(node.left);
+
+      if (node.right) nodeQueue.push(node.right);
+    }
+    result.push(levelList);
+    isOrderLeft = !isOrderLeft;
+  }
+
+  return result;
+}
