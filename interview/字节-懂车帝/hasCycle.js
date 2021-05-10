4a926fd57dee856c13b2085b55d20d3a5c061b90
xumaozeng <15611895341@163.com>
2021-05-10 09:27:24 +0800

    判断链表有环

---
+++ interview/字节-懂车帝/hasCycle.js
@@ -0,0 +1,17 @@
+/**
+ * 判断链表是否有环
+ */
+
+function hasCycle(head) {
+  if (!head) return false;
+  let slow = head,
+    fast = head;
+  while (fast && fast.next) {
+    fast = fast.next.next;
+    slow = slow.next;
+    // 代表有环
+    if (slow === fast) return true;
+  }
+
+  return false;
+}
