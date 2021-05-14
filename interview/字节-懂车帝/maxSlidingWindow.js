5c48ee041972f1de49a96b853cc49a5593c80859
xumaozeng <15611895341@163.com>
2021-05-14 09:54:42 +0800

    滑动窗口

---
+++ interview/字节-懂车帝/maxSlidingWindow.js
@@ -0,0 +1,25 @@
+/**
+ * 滑动窗口最大值
+ */
+
+function maxSlidingWindow(nums, k) {
+  const win = [];
+  const result = [];
+  if (k === 0) return [];
+  for (let i = 0; i < nums.length; i++) {
+    if (i - win[0] > k - 1) win.shift();
+
+    let j = win.length - 1;
+    while (j >= 0 && nums[win[j]] <= nums[i]) {
+      j--;
+      win.pop();
+    }
+    win.push(i);
+    if (i >= k - 1) result.push(nums[win[0]]);
+  }
+  return result;
+}
+
+// test
+const nums = [1, 3, -1, -3, 5, 3, 6, 7];
+console.log(maxSlidingWindow(nums, 3));
