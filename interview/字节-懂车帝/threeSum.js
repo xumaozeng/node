82a28962e4fe3ba294f9ae27588aab13f94640c2
xumaozeng <15611895341@163.com>
2021-05-25 12:30:24 +0800

    复习算法-三数之和

--- interview/字节-懂车帝/threeSum.js
+++ interview/字节-懂车帝/threeSum.js
@@ -3,29 +3,21 @@
  */
 
 function threeSum(nums) {
-  const result = [];
-  const len = nums.length;
-  if (len < 3) return [];
-  // 排序
   nums.sort((a, b) => a - b);
-  for (let i = 0; i < len; i++) {
-    // 如果当前数字大于0，则三数之和一定大于0,结束循环
-    if (nums[i] > 0) break;
-    // 去重
-    if (i > 0 && nums[i] === nums[i - 1]) continue;
+  const result = [];
+  if (nums.length < 3 || nums[0] > 0) return [];
+  for (let i = 0; i < nums.length - 2; i++) {
     let left = i + 1,
-      right = len - 1;
+      right = nums.length - 1;
+    if (i > 0 && nums[i] === nums[i - 1]) continue;
     while (left < right) {
-      const sum = nums[i] + nums[left] + nums[right];
+      let sum = nums[left] + nums[i] + nums[right];
       if (sum === 0) {
-        result.push([nums[i], nums[left], nums[right]]);
-        // 去重
-        while (left < right && nums[left] === nums[left + 1]) left++;
-        while (left < right && nums[right] === nums[right - 1]) right--;
-        left++;
-        right--;
-      } else if (sum < 0) left++;
-      else right--;
+        result.push([nums[left++], nums[i], nums[right--]]);
+        while (nums[left] === nums[left - 1]) left++;
+        while (nums[right] === nums[right + 1]) right--;
+      } else if (sum > 0) right--;
+      else left++;
     }
   }
   return result;
