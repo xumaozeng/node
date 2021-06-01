b73f3788fc5f177ba57947ae1fe60eb1777ee4ee
xumaozeng <15611895341@163.com>
2021-06-01 09:35:52 +0800

    前中序构造二叉树

--- interview/字节-懂车帝/buildTree.js
+++ interview/字节-懂车帝/buildTree.js
@@ -3,7 +3,7 @@
  */
 
 function buildTree(preorder, inorder) {
-  if (inorder.length === 0) return null;
+  if (preorder.length === 0) return null;
   const root = new TreeNode(preorder[0]);
   const mid = inorder.indexOf(preorder[0]);
   root.left = buildTree(preorder.slice(1, mid + 1), inorder.slice(0, mid));
