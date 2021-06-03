575a95cafb82cf2ebacb87a7963ca34afc9c7a36
xumaozeng <15611895341@163.com>
2021-06-03 16:07:44 +0800

    反转链表

---
+++ interview/开课吧-算法训练营/reverseList.js
@@ -0,0 +1,15 @@
+/**
+ * 翻转链表
+ */
+
+function reverseList(head) {
+  let pre = null;
+  let cur = head;
+  while (cur) {
+    let next = cur.next;
+    cur.next = pre;
+    pre = cur;
+    cur = next;
+  }
+  return pre;
+}
