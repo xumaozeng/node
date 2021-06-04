5e20a9e08b36e757760499372eebe2ebdaafb45a
xumaozeng <15611895341@163.com>
2021-06-04 15:44:02 +0800

    二叉树的最近公共祖先

--- interview/开课吧-算法训练营/lowestCommonAncestor.js
+++ interview/开课吧-算法训练营/lowestCommonAncestor.js
@@ -29,3 +29,14 @@ function lowestCommonAncestor2(root, p, q) {
     }
   }
 }
+
+// 变种：二叉树的最近公共祖先
+function lowestCommonAncestor(root, p, q) {
+  // 递归遍历
+  if (!root || root === p || root === q) return root;
+  const left = lowestCommonAncestor(root.left, p, q);
+  const right = lowestCommonAncestor(root.right, p, q);
+  if (left && right) return root;
+  if (left) return left;
+  return right;
+}
