8c915192ccb631756d30c88e8d88954be877e254
xumaozeng <15611895341@163.com>
2021-06-11 11:13:08 +0800

    两数之和

---
+++ interview/字节-懂车帝/twoSum.js
@@ -0,0 +1,15 @@
+/**
+ * 两数之和
+ */
+
+function twoSum(nums, target) {
+  const map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    const num = nums[i];
+    if (map.has(target - num)) {
+      return [map.get(target - num), i];
+    }
+    map.set(num, i);
+  }
+  return [];
+}
