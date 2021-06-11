b47b6f9b72fcfaa42c45c1721cbad5a5d05d986a
xumaozeng <15611895341@163.com>
2021-06-11 10:29:26 +0800

    锯齿形层次遍历

--- interview/字节-懂车帝/zigzagLevelOrder.js
+++ interview/字节-懂车帝/zigzagLevelOrder.js
@@ -4,16 +4,12 @@
 
 function zigzagLevelOrder(root) {
   if (!root) return [];
-
   const result = [];
   const nodeQueue = [root];
-
   let isOrderLeft = true;
-
   while (nodeQueue.length) {
-    let levelList = [];
+    const levelList = [];
     const size = nodeQueue.length;
-
     for (let i = 0; i < size; i++) {
       const node = nodeQueue.shift();
       if (isOrderLeft) {
@@ -22,12 +18,10 @@ function zigzagLevelOrder(root) {
         levelList.unshift(node.val);
       }
       if (node.left) nodeQueue.push(node.left);
-
       if (node.right) nodeQueue.push(node.right);
     }
     result.push(levelList);
     isOrderLeft = !isOrderLeft;
   }
-
   return result;
 }
