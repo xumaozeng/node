7811163c000ffa8fbfbf8d533388efc9a940756a
xumaozeng <15611895341@163.com>
2021-06-12 15:22:28 +0800

    async-await

--- interview/async-await.js
+++ interview/async-await.js
@@ -1,37 +1,40 @@
 /**
  * 手写async-await
+ *实现原理是：自执行器+generator函数包装在一个函数里
  */
 
-function asyncToGenerator(generatorFunc) {
-  // 返回的是一个函数
-  return function () {
-    // 先调用generator函数，生成迭代器
-    const gen = generatorFunc.apply(this, arguments);
-    // 返回一个promise
-    return new Promise((resolve, reject) => {
-      // 内部定义一个step函数，用来一步一步的跨越yield阻碍
-      function step(key, arg) {
-        let generatorResult;
+function async_await(args) {
+  // spawn-自动执行器
+  // genF是generator函数
+  return function spawn(genF) {
+    return new Promise(function (resolve, reject) {
+      const gen = genF();
+      function step(nextF) {
+        let next;
         try {
-          generatorResult = gen[key](arg);
-        } catch (error) {
-          return reject(error);
+          next = nextF();
+        } catch (e) {
+          return reject(e);
         }
-        const { value, done } = generatorResult;
-        if (done) {
-          return resolve(value);
-        } else {
-          return Promise.resolve(value).then(
-            function onResolve(val) {
-              step("next", val);
-            },
-            function onReject(err) {
-              step("throw", err);
-            }
-          );
+        if (next.done) {
+          return resolve(next.value);
         }
+        Promise.resolve(next.value).then(
+          function (v) {
+            step(function () {
+              return gen.next(v);
+            });
+          },
+          function (e) {
+            step(function () {
+              return gen.throw(e);
+            });
+          }
+        );
       }
-      step("next");
+      step(function () {
+        return gen.next(undefined);
+      });
     });
   };
 }
