b9e2efe78e445e442361ae19941bf2d443bbd96f
xumaozeng <15611895341@163.com>
2021-06-14 22:31:23 +0800

    二叉树的后序遍历

--- interview/字节-懂车帝/postorderTraversal.js
+++ interview/字节-懂车帝/postorderTraversal.js
@@ -4,15 +4,14 @@
 
 function postorderTraversal(root) {
   // 迭代遍历-左右根
+  if (!root) return [];
   const result = [];
   const stack = [root];
   while (stack.length) {
     const node = stack.pop();
-    if (node) {
-      result.unshift(node.val);
-      if (node.left) stack.push(node.left);
-      if (node.right) stack.push(node.right);
-    }
+    result.unshift(node.val);
+    if (node.left) stack.push(node.left);
+    if (node.right) stack.push(node.right);
   }
   return result;
 }
