79fe18e8312e3335997c2639e0cfe58e7f4529b2
xumaozeng <15611895341@163.com>
2021-06-25 14:53:02 +0800

    字符串相加

---
+++ interview/字节-抖音电商/addStrings.js
@@ -0,0 +1,20 @@
+/**
+ * 字符串相加
+ */
+
+function addStrings(num1, num2) {
+  let i = num1.length - 1,
+    j = num2.length - 1,
+    add = 0;
+  const result = [];
+  while (i >= 0 || j >= 0 || add !== 0) {
+    const x = i >= 0 ? num1.charAt(i) - "0" : 0;
+    const y = j >= 0 ? num2.charAt(j) - "0" : 0;
+    const sum = x + y + add;
+    result.push(sum % 10);
+    add = Math.floor(sum / 10);
+    i -= 1;
+    j -= 1;
+  }
+  return result.reverse().join("");
+}
