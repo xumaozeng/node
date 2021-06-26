8e840cb0cc16e66f174240a3bb30a120908a72d9
xumaozeng <15611895341@163.com>
2021-06-26 15:01:46 +0800

    压缩字符串

---
+++ interview/字节-抖音电商/compress.js
@@ -0,0 +1,25 @@
+/**
+ * 压缩字符串
+ */
+
+function compress(chars) {
+  let ch = chars[0];
+  let size = chars.length;
+  let i = 0,
+    j = 0;
+  while (size) {
+    if (ch !== chars[0]) {
+      chars.push(ch);
+      if (j > 1) chars.push(...(j + ""));
+      j = 0;
+      ch = chars[0];
+    }
+    chars.shift();
+    j++;
+    size--;
+  }
+  // 加上最后的字符
+  chars.push(ch);
+  if (j > 1) chars.push(...(j + ""));
+  return chars.length;
+}
