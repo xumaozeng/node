e406890e7230debfbb44c740e6d1a029913aa408
xumaozeng <15611895341@163.com>
2021-06-27 10:09:04 +0800

    二叉树最小深度

---
+++ interview/开课吧-算法训练营/minDepth.js
@@ -0,0 +1,12 @@
+/**
+ * 二叉树的最小深度
+ */
+
+function minDepth(root) {
+  if (!root) return 0;
+  if (!root.left || !root.right) return 1;
+  let min = Number.MAX_VALUE;
+  if (root.left) min = Math.min(minDepth(root.left), min);
+  if (root.right) min = Math.min(minDepth(root.right), min);
+  return min + 1;
+}
