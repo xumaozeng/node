011e825a0ee21704ecb855696a995fc9e30420f4
xumaozeng <15611895341@163.com>
2021-07-06 12:46:47 +0800

    百度-环形链表

---
+++ interview/百度-AI产品研发部/hasCycle.js
@@ -0,0 +1,14 @@
+/**
+ * 环形链表
+ */
+
+function hasCycle(head) {
+  let fast = head,
+    slow = head;
+  while (fast && fast.next) {
+    fast = fast.next.next;
+    slow = slow.next;
+    if (fast === slow) return true;
+  }
+  return false;
+}
