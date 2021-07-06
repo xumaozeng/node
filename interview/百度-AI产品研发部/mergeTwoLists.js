f516b255214f78dd56652eecb614d3a6b2165886
xumaozeng <15611895341@163.com>
2021-07-06 13:43:34 +0800

    合并两个有序链表

---
+++ interview/百度-AI产品研发部/mergeTwoLists.js
@@ -0,0 +1,18 @@
+/**
+ * 合并两个排序的链表
+ */
+
+function mergeTwoLists(l1, l2) {
+  if (!l1) return l2;
+  if (!l2) return l1;
+
+  let head;
+  if (l1.val < l2.val) {
+    head = l1;
+    head.next = mergeTwoLists(l1.next, l2);
+  } else {
+    head = l2;
+    head.next = mergeTwoLists(l1, l2.next);
+  }
+  return head;
+}
