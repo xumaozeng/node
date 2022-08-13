3aadbf55ef1e4c237c82e73c5a4a6cf30362a04c
xumaozeng <15611895341@163.com>
2022-08-13 16:44:23 +0800

    node-模块化

---
+++ study/module_1.js
@@ -0,0 +1,14 @@
+/**
+ * CommonJS模块化的使用
+ */
+
+function add(a, b) {
+  return a + b;
+}
+
+function mul(a, b) {
+  return a * b;
+}
+
+exports.add = add;
+exports.mul = mul;
---
+++ study/module_2.js
@@ -0,0 +1,3 @@
+const md = require("./module_1");
+
+console.log(md);
