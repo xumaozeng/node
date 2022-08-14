f47b66f5e0994aab7726a391a300420120d63fcd
xumaozeng <15611895341@163.com>
2022-08-14 18:39:12 +0800

    module.exports与exports

--- study/module_1.js
+++ study/module_1.js
@@ -1,5 +1,8 @@
 /**
  * CommonJS模块化的使用
+ * exports和module.exports是一样的，不过：
+ * exports不能直接赋值，直接复制改变了这个变量
+ * module.exports可以.方法，也可以直接赋值
  */
 
 function add(a, b) {
@@ -10,5 +13,4 @@ function mul(a, b) {
   return a * b;
 }
 
-exports.add = add;
-exports.mul = mul;
+module.exports = { add, mul };
